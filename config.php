467ebe481a9ba8e3b08b1881aa9993189b809b8a
irismtz <51760990+irismtz@users.noreply.github.com>
2021-08-12 01:32:24 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-$con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "inventariousuarios.mysql.database.azure.com", "irismtz@inventariousuarios", "Martinez19", "crud_basico", 3306);
+$mysqli = new mysqli('inventariousuarios.mysql.database.azure.com', 'irismtz@inventariousuarios', 'Martinez19', 'crud_basico');
 ?>
